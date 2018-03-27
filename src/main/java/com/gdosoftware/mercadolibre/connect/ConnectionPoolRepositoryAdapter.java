f99db418423a3bc7ac7d9e8ee2cea02dfe4caf40
Administrador <gdosoftware@gmail.com>
2018-03-27 12:00:26 -0300

    ConnectionPoolRepositoryAdapter

---
+++ src/main/java/com/gdosoftware/mercadolibre/connect/ConnectionPoolRepositoryAdapter.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gdosoftware.mercadolibre.connect;
+
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+/**
+ *
+ * @author Daniel Gago
+ */
+public abstract class ConnectionPoolRepositoryAdapter implements ConnectionPoolRepository{
+    
+    private final String  KEY="Thy75123Swk71658";
+  
+
+    public  String encrypt(String data) throws Exception {
+        Key key = new SecretKeySpec(KEY.getBytes(), "AES");
+        Cipher c = Cipher.getInstance("AES");
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encVal = c.doFinal(data.getBytes());
+        return new BASE64Encoder().encode(encVal);
+    }
+    
+    public  String decrypt(String encryptedData) throws Exception {
+        Key key = new SecretKeySpec(KEY.getBytes(), "AES");
+        Cipher c = Cipher.getInstance("AES");
+        c.init(Cipher.DECRYPT_MODE, key);
+        byte[] decordedValue = new BASE64Decoder().decodeBuffer(encryptedData);
+        byte[] decValue = c.doFinal(decordedValue);
+        return new String(decValue);
+    }
+   
+   
+}
